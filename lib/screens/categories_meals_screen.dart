caa57a46244a2563235cbd00c2b3da6a29cd3f29
Anderson Mendes <developer.andersonmendes@gmail.com>
2022-06-20 23:08:17 -0300

    Aula 190 - Finalizada

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
 import 'package:meals/data/dummy_dart.dart';
 
 import '../models/category.dart';
@@ -20,7 +21,7 @@ class CategoriesMealsScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: categoryMeals.length,
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(meal: categoryMeals[index]);
         },
       ),
     );
